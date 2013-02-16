a7146db8e15e8a7613204afc21e21f01714b52f8
Fuad Saud <fuadksd@gmail.com>
2013-02-16 04:56:18 -0200

    Add tests for invalid URI's

--- spec/rockt_spec.rb
+++ spec/rockt_spec.rb
@@ -4,4 +4,22 @@ describe Rockt do
   it 'should have a version' do
     expect(Rockt::VERSION).to be_an(String)
   end
+
+  context 'given invalid URI' do
+    context "it's a path for existing file" do
+      it 'should not raise exception' do
+        expect { Rockt.launch('/bin') }.not_to raise_error(Rockt::NoSuitableApplication)
+      end
+    end
+
+    context "it's not a path for existing file" do
+      it 'should raise exception' do
+        expect { Rockt.launch('path-to-non-existing-file') }.to raise_error(Rockt::NoSuitableApplication)
+      end
+    end
+  end
+
+  context 'given valid URI' do
+    pending
+  end
 end
