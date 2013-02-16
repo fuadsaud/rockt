7117d372f827ced24168756f42966934453d8af2
Fuad Saud <fuadksd@gmail.com>
2013-02-15 23:32:01 -0200

    Refactor some tests

--- spec/rockt_spec.rb
+++ spec/rockt_spec.rb
@@ -5,13 +5,15 @@ describe Rockt do
     expect(Rockt::VERSION).to be_an(String)
   end
 
-  it 'should fire current environment application launcher' do
-    expect(Rockt.detect.commands).to include(Rockt.launch('http://github.com/fuadsaud'))
-  end
-end
-
-describe Rockt::Environment do
   it 'should know at least one environment' do
     expect(Rockt::Environment::KNOWN_ENVIRONMENTS).not_to be_empty
+  end
+
+  it 'should correctly detect current environment' do
+    (Rockt::detect.regex).should match(RbConfig::CONFIG['host_os'])
+  end
+
+  it 'should fire current environment application launcher' do
+    expect(Rockt.detect.commands).to include(Rockt.launch('http://github.com/fuadsaud'))
   end
 end
